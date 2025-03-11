9e981e8c95972873b556fc24e5dd123e987badc8
Ren Tamdang <tamdangren@gmail.com>
2025-03-11 15:28:43 +0800

    Initial push to put all that shit in

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ function checkLogin() {
     let password = document.getElementById("password").value;
 
     if (userID === "admin" && password === "123") {
-        window.location.href = "material_requests.html";  
+        // window.location.href = "material_requests.html"; 
+        redirect('Admin View/material_requests.html');
     } else if (userID === "driver" && password === "345") {
-        window.location.href = "trip_ticket_create.html";  
+        // window.location.href = "trip_ticket_create.html";  
+        redirect("Driver View/trip_ticket_create.html");
     } else {
         alert("Invalid ID or Password. Try again.");
     }
